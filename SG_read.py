170ec8cb755170e758b76e6d0fe74c5e56adeaba
出町史夏 <demachifumika@MacbookAir.local>
2021-09-22 17:03:34 +0900

    changed file

--- SG_read.py
+++ SG_read.py
@@ -3,23 +3,26 @@ from std_msgs.msg import Int32
 from std_msgs.msg import Float64
 
 name_list = []
-for n in ["100ghz","200ghz"]:
-    for m in ["1st", "2nd_upper", "2nd_lower"]:
-        name_ = "sg_{}_{}".format(n,m)
+for band in ["100ghz","200ghz"]:
+    for stage in ["1st", "2nd_upper", "2nd_lower"]:
+        name_ = "sg_{}_{}".format(band,stage)
         name_list.append(name_)
 
 def reader_freq():
     for name in name_list:
-        rospy.init_node('e8257d')
-        pub_freq = rospy.Publisher(name, Float64, que_size=1)
-        pub_power = rospy.Publisher(name, Float64, que_size=1)
-        pub_onoff = rospy.Publisher(name, Int32, que_size=1)
+        rospy.init_node('e8257d_trigger')
+        pub_freq = rospy.Publisher("{}_freq".format(name), Float64, queue_size=1)
+        pub_power = rospy.Publisher("{}_power".format(name), Float64, queue_size=1)
+        pub_onoff = rospy.Publisher("{}_onoff".format(name), Int32, queue_size=1)
         rate = rospy.Rate(0.2)
+        
+        while not rospy.is_shutdown():
+            msg_freq = Float64()
+            msg_power = Float64()
+            msg_onoff = Int32()
 
-        msg_freq = Float64
-        msg_power = Float64
-        msg_onoff = Int32
+            pub_freq.publish(msg_freq)
+            pub_power.publish(msg_power)
+            pub_onoff.publish(msg_onoff)
 
-        pub_freq.publish(msg_freq)
-        pub_power.publish(msg_power)
-        pub_onoff.publish(msg_onoff)
+            rate.sleep()
